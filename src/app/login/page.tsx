057246429d2f28fd7b8c72e7e9fd8e3d95b6b223
emuhammadali <siddali662@gmail.com>
2026-09-20 21:17:16 +0500

    feat: update login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -139,7 +139,7 @@ export default function LoginPage() {
             </button>
           </form>
 
-          <div className="mt-6">
+          {/* <div className="mt-6">
             <div className="relative">
               <div className="absolute inset-0 flex items-center">
                 <div className="w-full border-t border-slate-200" />
@@ -172,7 +172,7 @@ export default function LoginPage() {
                 </button>
               ))}
             </div>
-          </div>
+          </div> */}
 
           <p className="mt-6 text-center text-sm text-slate-500">
             Don&apos;t have an account?{" "}
